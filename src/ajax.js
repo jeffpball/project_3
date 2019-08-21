8ae5055d2e03fb84eb5f528d34b4beac87f1576f
Nick Bever <nicholasbever@D-10-19-173-30.dhcp4.washington.edu>
2019-08-20 21:41:32 -0700

    added more functionality to axios request

--- src/ajax.js
+++ src/ajax.js
@@ -1,6 +1,9 @@
 const axios = require("axios");
 const eventArray = [];
-axios.get("https://api.seatgeek.com/2/events?performers.id=13719&per_page=3&client_id=MTIwNzV8MTM2NTQ1MDQyMg").then(function(response) {
+const searchedFor = "Minneapolis, MN"
+
+const querySeatGeek = () => {
+    axios.get("https://api.seatgeek.com/2/events?performers.id=13719&per_page=3&client_id=MTIwNzV8MTM2NTQ1MDQyMg").then(function(response) {
     const events = response.data.events;
     for (event in events) {
         let eventDict = {};
@@ -8,6 +11,25 @@ axios.get("https://api.seatgeek.com/2/events?performers.id=13719&per_page=3&clie
         eventDict.eventId = events[event].id;
         eventArray.push(eventDict);
     }
-    console.log(eventArray);
-});
+    return eventArray
+}).then(function(res) {
+        for (event in eventArray) {
+            if (eventArray[event].venue === searchedFor) {
+                return eventArray[event].eventId
+            } else {
+                return "Sorry, not in there"
+            }
+        }
+    });
+};
+
+// let x = querySeatGeek();
+// console.log(x);
+
+console.log(querySeatGeek());
+
+
+
+
+
 
