779e87a904a746a9581eebcee19e2ffda68061f8
Nate Bennett <26212562+NateBennett9@users.noreply.github.com>
2019-08-27 16:37:31 -0700

    updated cards

--- client/src/components/EventCard/index.js
+++ client/src/components/EventCard/index.js
@@ -2,24 +2,28 @@ import React from 'react'
 import './style.css'
 import CardDeck from 'react-bootstrap/CardDeck'
 import Card from 'react-bootstrap/Card'
+import post from './postmalone.JPG'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
 
 function EventCard() {
     return (
+        <Container>
         <CardDeck>
             <Card>
-                <Card.Img variant="top" src="holder.js/100px160" roundedCircle/>
+            <Card.Img variant="top" src="https://static.seattletimes.com/wp-content/uploads/2018/04/173623-780x592.jpg" />
                 <Card.Body>
                     <Card.Title>Venue</Card.Title>
                     <Card.Text>
                         Tacoma Dome
-      </Card.Text>
+                    </Card.Text>
                 </Card.Body>
                 <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                    
                 </Card.Footer>
             </Card>
             <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Img variant="top" src="https://blog.hootsuite.com/wp-content/uploads/2017/06/social-media-content-calendar-940x470.jpg" />
                 <Card.Body>
                     <Card.Title>Date</Card.Title>
                     <Card.Text>
@@ -27,34 +31,35 @@ function EventCard() {
                     </Card.Text>
                 </Card.Body>
                 <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                    
                 </Card.Footer>
             </Card>
             <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Img variant="top" src="https://sosyalmedya.co/wp-content/uploads/2016/08/pexels-photo-100733-large-940x470.jpeg" />
                 <Card.Body>
                     <Card.Title>Time</Card.Title>
                     <Card.Text>
                         8:00pm
-      </Card.Text>
+                    </Card.Text>
                 </Card.Body>
                 <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                    
                 </Card.Footer>
             </Card>
             <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Img variant="top" src="http://www.simplesend.com/simple/uploadedimages/000397/SeattleKnow.jpg" />
                 <Card.Body>
                     <Card.Title>City</Card.Title>
                     <Card.Text>
-                       Seattle, WA
-      </Card.Text>
+                        Seattle, WA
+                    </Card.Text>
                 </Card.Body>
                 <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                    
                 </Card.Footer>
             </Card>
         </CardDeck>
+        </Container>
     );
 }
 
