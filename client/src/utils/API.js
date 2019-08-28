8caaaa519151acbd4206702ea27c319273d75dd2
Jeff Ball <jeffball@Jeffs-MacBook-Pro.local>
2019-08-27 21:16:27 -0700

    Add CRUD operations to API.js in util folder - getLocations, getSavedInfo, deleteSavedInfo, and saveInfo. Add info folder to routes. Add Info.js to info folder in routes.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 export default {
-  // Gets books from the Google API
-  getBooks: function(q) {
-    return axios.get("/api/google", { params: { q: "title:" + q } });
+  // Gets event locations from the SeatGeekAPI by typing an artist name into the arist search input field and clicking search 
+  getLocations: function(q) {
+    return axios.get("/api/seatGeekAPI/SeatGeekAPI", { params: { q: "location:" + q } });
   },
-  // Gets all saved books
-  getSavedBooks: function() {
+  // Gets all saved info
+  getSavedInfo: function() {
     return axios.get("/api/books");
   },
-  // Deletes the saved book with the given id
-  deleteBook: function(id) {
+  // Deletes the saved info with the given id
+  deleteSavedInfo: function(id) {
     return axios.delete("/api/books/" + id);
   },
-  // Saves an book to the database
-  saveBook: function(bookData) {
+  // Saves info to the database
+  saveInfo: function(bookData) {
     return axios.post("/api/books", bookData);
   }
 };
