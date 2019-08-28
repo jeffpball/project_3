741b2e41b7d4ff061f7ee54a644a2ec9a0910293
Jeff Ball <jeffball@Jeffs-MacBook-Pro.local>
2019-08-27 21:37:44 -0700

    Add getPriceSeatGeek, getPriceStubHub, and  getPriceTicketMaster to API.js in utils folder

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,16 +5,26 @@ export default {
   getLocations: function(q) {
     return axios.get("/api/seatGeekAPI/SeatGeekAPI", { params: { q: "location:" + q } });
   },
+  // Gets event locations from the SeatGeekAPI by typing an artist name into the arist search input field and clicking search 
+  getPriceSeatGeek: function(q) {
+    return axios.get("/api/seatGeekAPI/SeatGeekAPI", { params: { q: "price:" + q } });
+  },
+  getPriceStubHub: function(q) {
+    return axios.get("/api/stubHubAPI/StubHubAPI", { params: { q: "price:" + q } });
+  },
+  getPriceTicketMaster: function(q) {
+    return axios.get("/api/ticketMasterAPI/TicketMasterAPI", { params: { q: "price:" + q } });
+  },
   // Gets all saved info
   getSavedInfo: function() {
-    return axios.get("/api/books");
+    return axios.get("/api/info/Info");
   },
   // Deletes the saved info with the given id
   deleteSavedInfo: function(id) {
-    return axios.delete("/api/books/" + id);
+    return axios.delete("/api/info/Info" + id);
   },
   // Saves info to the database
-  saveInfo: function(bookData) {
-    return axios.post("/api/books", bookData);
+  saveInfo: function(ticketData) {
+    return axios.post("/api/info/Info", ticketData);
   }
 };
